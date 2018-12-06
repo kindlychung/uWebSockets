0e2dbdf90171fb1053118c48fd87aecf5f70fcb1
Kaiyin Zhong <kindlychung@gmail.com>
2018-12-06 10:09:58 +0100

    fix lib name

--- conanfile.py
+++ conanfile.py
@@ -7,19 +7,24 @@ import shutil
 
 
 class uwebsocketConan(ConanFile):
-    name = "uwebsocket"
+    name = "uwebsockets"
     version = "0.14.9"
     license = "Apache 2.0"
     author = "uNetworking"
     url = "https://github.com/kindlychung/uWebSockets"
     description = "High performance WebSockets"
-    topics = ("cpp", "network", "sockets")
+    topics = ("cpp", "network", "sockets", "websocket", "ws")
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
     default_options = "shared=False"
     requires = ("OpenSSL/1.1.1a@jzien/dev", "usockets/0.0.6@jzien/dev")
     generators = "cmake"
-    exports_sources = "src/*"
+    exports_sources = "src/*", "CMakeLists.txt"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder=".")
+        cmake.build()
 
     def package(self):
         self.copy("*.h", dst="include/uWS", src="src", keep_path=False)
@@ -34,4 +39,4 @@ class uwebsocketConan(ConanFile):
         self.copy("*", dst="lib", src="lib")
 
     def package_info(self):
-        self.cpp_info.libs = ["usockets"]
+        self.cpp_info.libs = ["uwebsockets"]
